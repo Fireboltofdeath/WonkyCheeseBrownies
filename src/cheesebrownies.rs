52a087aabcbb8b502eeafab67a3734f2d894787b
Fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2020-03-27 16:07:50 -0400

    Add naive rust implementation

---
+++ src/cheesebrownies.rs
@@ -0,0 +1,25 @@
+use std::cell::RefCell;
+
+struct Consumer {
+    sauces: RefCell<Vec<String>>
+}
+
+impl Consumer {
+    fn new() -> Self {
+        Consumer {
+            sauces: RefCell::new(Vec::new())
+        }
+    }
+    
+    fn eat(&self) {
+        println!("I eat brownie with sauces: {:?}", self.sauces.borrow());
+	}
+	
+	fn sauce(&self, sauce: &str) {
+		self.sauces.borrow_mut().push(sauce.to_string());
+	}
+	
+	fn cheese(&self) {
+	    self.sauce("Cheese");
+	}
+}
